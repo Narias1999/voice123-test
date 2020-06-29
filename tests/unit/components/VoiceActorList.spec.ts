714cff651c533aa9d2564400cbf5502017ae56c8
narias1999 <nicolasarias870@gmail.com>
2020-06-29 17:56:12 -0500

    linting

--- tests/unit/components/VoiceActorList.spec.ts
+++ tests/unit/components/VoiceActorList.spec.ts
@@ -1,14 +1,14 @@
 import { mount } from "@vue/test-utils";
 import VoiceActorList from "@/components/VoiceActorList.vue";
 import VoiceActorCard from "@/components/VoiceActorCard.vue";
-import { actor } from "./../../__mocks__/actor"
+import { actor } from "./../../__mocks__/actor";
 
 describe("VoiceActorList", () => {
   const actors = 20;
   const wrapper = mount(VoiceActorList, {
     propsData: {
       actors: new Array(20).fill(actor),
-      keyword: ''
+      keyword: ""
     }
   });
 
